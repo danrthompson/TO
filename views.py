5b17e1d0eab7e58b6cc3c3ee820cb512e840c547
Dan Thompson <dant1@stanford.edu>
2011-05-15 22:42:20 -0700

    actually developing

--- views.py
+++ views.py
@@ -36,6 +36,6 @@ def check_auth(request):
 def private_page(request):
     name=request.GET['user']
     loginUser = Profile.objects.get(name)
-    wants_css = loginUser.want_css
-    return render_to_response('private_page.html', {'name': name}, {'wants_css': wants_css})
+#    wants_css = loginUser.want_css
+    return render_to_response('private_page.html', {'name': name}, {'wants_css': True})
 
