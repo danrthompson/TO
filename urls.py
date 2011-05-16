aa4520053d5f2f624a5826da478a5ff7c4e1551f
Dan Thompson <dant1@stanford.edu>
2011-05-16 01:34:14 -0700

    grr

--- urls.py
+++ urls.py
@@ -17,8 +17,8 @@ urlpatterns = patterns('',
 (r'^private_page/$', 'views.private_page'),
 
     # Uncomment the admin/doc line below to enable admin documentation:
-url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-url(r'^admin/', include(admin.site.urls)),
+(r'^admin/', include(admin.site.urls)),
 )
